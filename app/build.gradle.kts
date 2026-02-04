5ec6869adcb6d66680de7cbd273bd7fa7804aa83
o-benjamin <06enjamin@gmail.com>
2026-02-04 22:43:24 +0900

    chore: Bump app version to 1.1.0
    Updates the application's version information.
    
    This commit includes the following changes:
    - Increments `versionCode` from 6 to 7.
    - Sets `versionName` to "1.1.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.fansauchiwa"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.0_0.3.0"
+        versionCode = 7
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
