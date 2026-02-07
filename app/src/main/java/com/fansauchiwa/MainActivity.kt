9ed90899cc9fb76207386c1f4ab3f6a4682ac8b3
o-benjamin <06enjamin@gmail.com>
2026-02-07 23:40:27 +0900

    release/1.2.0[feat]: Enhance Status Bar and Navigation Bar Styles
    * Update status bar style to use transparent colors
    * Add navigation bar style with transparent colors

--- app/src/main/java/com/fansauchiwa/MainActivity.kt
+++ app/src/main/java/com/fansauchiwa/MainActivity.kt
@@ -19,7 +19,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.light(R.color.transparent, R.color.transparent)
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
         )
 
         // 確実に広告を表示するため、アプリ起動時に広告を事前ロード
