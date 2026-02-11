ba7a37e9b7d1734fb2e81027b7cfe7b56e32c1f8
o-benjamin <06enjamin@gmail.com>
2026-02-11 11:51:04 +0900

    release/1.2.0[feat]: Update Version Code and Name for Release 1.2.0
    * Increment versionCode to 9
    * Update versionName to "1.2.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.fansauchiwa"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.1.0"
+        versionCode = 9
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
