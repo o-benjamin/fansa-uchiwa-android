d3ad0688f83cf6cc11d39e54fc9c91645ef3650c
o-benjamin <06enjamin@gmail.com>
2026-02-15 14:25:20 +0900

    release/1.4.0[chore]: Update Version Code and Name for Release
    * Increment versionCode to 11
    * Update versionName to "1.4.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.fansauchiwa"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.3.0"
+        versionCode = 11
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
