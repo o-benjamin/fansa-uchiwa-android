f749f786a7d24a0e588815afc148c6e13ba8e9e7
o-benjamin <06enjamin@gmail.com>
2026-03-07 00:29:54 +0900

    release/1.7.0[chore]: Update Version Code and Name in Build Configuration
    * Increment versionCode to 14
    * Update versionName to "1.7.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.fansauchiwa"
         minSdk = 29
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.6.0"
+        versionCode = 14
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
