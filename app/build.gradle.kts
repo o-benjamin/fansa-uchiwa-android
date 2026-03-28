cdff5425204e90394d429a3b5d4023d784f67058
o-benjamin <06enjamin@gmail.com>
2026-03-28 21:55:03 +0900

    release/v2.2.0[feat]: Update Version Code and Name for Release 2.2.0
    * Increment versionCode to 19
    * Update versionName to "2.2.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.fansauchiwa"
         minSdk = 29
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.1.0"
+        versionCode = 19
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
