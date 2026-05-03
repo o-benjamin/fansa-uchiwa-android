043d2e75741e507dd29076814fc5bc734e3a3ecc
o-benjamin <06enjamin@gmail.com>
2026-05-03 19:52:47 +0900

    release/v2.2.0[refactor]: Refactor EditCommonComponents for Consistency
    * Replace hardcoded spacing and size values with constants
    * Update padding and size for improved layout consistency
    * Change haptic feedback retrieval method in MainActivity

--- app/src/main/java/com/fansauchiwa/ui/DecorationColors.kt
+++
@@ -1,7 +0,0 @@
-package com.fansauchiwa.ui
-
-@Deprecated(
-    message = "Use com.fansauchiwa.data.DecorationColors for shared decoration color definitions.",
-    replaceWith = ReplaceWith("DecorationColors", "com.fansauchiwa.data.DecorationColors")
-)
-typealias DecorationColors = com.fansauchiwa.data.DecorationColors
