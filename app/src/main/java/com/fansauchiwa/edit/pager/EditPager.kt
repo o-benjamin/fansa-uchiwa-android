f681b70a6f033b08933bb25d4d6c64f0c9e4e7ee
o-benjamin <06enjamin@gmail.com>
2026-05-09 14:55:08 +0900

    ステッカーもぷくぷくになるよう対応

--- app/src/main/java/com/fansauchiwa/edit/pager/EditPager.kt
+++ app/src/main/java/com/fansauchiwa/edit/pager/EditPager.kt
@@ -46,6 +46,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
